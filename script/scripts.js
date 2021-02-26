56ca0d01c1d52518f0125f196915cd7e235dd99c
Jesus Villarreal <jesusvillarrealzavaleta@gmail.com>
2021-02-26 17:36:58 -0500

    Terminado en desktop

--- script/scripts.js
+++ script/scripts.js
@@ -1,16 +1,10 @@
 // Accordion
-const accordionItemHeaders = document.querySelectorAll(".accordion-item-header"
+const accordionItemHeaders = document.querySelectorAll(
+  ".accordion-item-header"
 );
 
 accordionItemHeaders.forEach((accordionItemHeader) => {
   accordionItemHeader.addEventListener("click", (event) => {
-    // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
-
-    // const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
-    // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
-    //   currentlyActiveAccordionItemHeader.classList.toggle("active");
-    //   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-    // }
 
     accordionItemHeader.classList.toggle("active");
     const accordionItemBody = accordionItemHeader.nextElementSibling;
@@ -23,7 +17,9 @@ accordionItemHeaders.forEach((accordionItemHeader) => {
 });
 
 
-// modal 
+
+
+
 
 
 // aaa
@@ -66,41 +62,42 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-let slider = document.querySelector(".slider")
-let sliderIndividual = document.querySelectorAll(".slider-item")
-let contador = 1;
-let width = sliderIndividual[0].clientWidth;
-let intervalo = 3000;
+// let slider = document.querySelector(".slider")
+// let sliderIndividual = document.querySelectorAll(".slider-item")
+// let contador = 1;
+// let width = sliderIndividual[0].clientWidth;
+// let intervalo = 3000;
 
-window.addEventListener("resize", function () {
-  let width = sliderIndividual[0].clientWidth;
-})
+// window.addEventListener("resize", function () {
+//   let width = sliderIndividual[0].clientWidth;
+// })
 
-setInterval (function (){
-  slides();
-}, intervalo);
+// setInterval (function (){
+//   slides();
+// }, intervalo);
 
-function slides() {
-  slider.style.transform = "translate("+(-width * contador)+"px)";
-  slider.style.transition = "transform .7s"
-  contador ++;
+// function slides() {
+//   slider.style.transform = "translate("+(-width * contador)+"px)";
+//   slider.style.transition = "transform .7s"
+//   contador ++;
 
-  if (contador == sliderIndividual.length){
-    setTimeout(function() {
-      slider.style.transform = "translate(0px)";
-      slider.style.transition = "transform .7s";
-      contador = 1; 
-    },1500)
-  }
-}
+//   if (contador == sliderIndividual.length){
+//     setTimeout(function() {
+//       slider.style.transform = "translate(0px)";
+//       slider.style.transition = "transform .7s";
+//       contador = 1; 
+//     },1500)
+//   }
+// }
 
-console.log("1");
+// modal 
+
 const buttonOpen = document.getElementById('open-modal')
 const buttonClose = document.getElementById('close-modal')
 const modal = document.getElementById('modal')
 const toggleActiveModal = () => modal.classList.toggle('active')
 
-buttonClose.addEventListener('click', toggleActiveModal);
-buttonOpen.addEventListener('click', toggleActiveModal);
+buttonClose.addEventListener('click', toggleActiveModal)
+buttonOpen.addEventListener('click', toggleActiveModal)
 
-console.log("2");
+
